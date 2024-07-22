459bd86a31a8a63f157eda6d05e1c975c8055259
Omer Keskin <keskinomerf@gmail.com>
2024-07-22 16:38:07 +0300

    StackView Attributes

--- StackViewProject/ViewController.swift
+++ StackViewProject/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 15
+
+        view.addSubview(stackView)
+
+        let spells = ["Expelliarmus", "Alohomora", "Lumos", "Expecto Patronum"]
+        for spell in spells {
+            let label = UILabel()
+            label.text = spell
+            label.textAlignment = .center
+            label.backgroundColor = .orange
+            label.textColor = .white
+            label.layer.cornerRadius = 5
+            label.font = .boldSystemFont(ofSize: 21)
+            label.layer.masksToBounds = true
+            stackView.addArrangedSubview(label)
+        }
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
-
